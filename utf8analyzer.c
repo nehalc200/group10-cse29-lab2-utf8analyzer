6becf2d5a6d23b17f3bdeeb1e5abbe6a19e3ddc7
Singh <ars023@ieng6-203.ucsd.edu>
2025-07-09 13:46:59 -0700

    added animal emojis

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -116,7 +116,63 @@ int main(int argc, char *argv[]) {
 		str4[i] = scopy[i];
 	}
 	str4[index4] = 0;
-	printf("Subtring of the first 6 codepoints: %s\n", str4);
+	printf("Substring of the first 6 codepoints: \"%s\"\n", str4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	char ans[strlen(argv[1])+1];
+	int index5 = 0;
+	int r = 0;
+	while(argv[1][index5] != 0){
+		uint32_t cp = 0;
+		int len = 0;
+		unsigned char byte = (unsigned char)argv[1][index5];
+		if (byte <= 0x7F) {
+            		len = 1;  
+        	} else if ((byte & 0xE0) == 0xC0) {
+            		len = 2;  
+        	} else if ((byte & 0xF0) == 0xE0) {
+            		len = 3;  
+        	} else if ((byte & 0xF8) == 0xF0) {
+            		len = 4;  
+        	} else {
+            		len = 1;  
+        	}
+		if (len == 1){        		
+			cp = argv[1][index5];
+		}
+    		else if (len == 2){
+        		cp = ((argv[1][index5] & 0x1F) << 6) | (argv[1][index5+1] & 0x3F);
+		}
+    		else if (len == 3){
+        		cp = ((argv[1][index5] & 0x0F) << 12) | ((argv[1][index5+1] & 0x3F) << 6) | (argv[1][index5+2] & 0x3F);
+		}
+    		else if (len == 4){
+        		cp = ((argv[1][index5] & 0x07) << 18) | ((argv[1][index5+1] & 0x3F) << 12) | ((argv[1][index5+2] & 0x3F) << 6) | (argv[1][index5+3] & 0x3F);
+		}
+		if ((cp >= 128000 && cp <= 128063) || (cp >= 129408 && cp <= 129710)){
+			for (int i = 0; i < len; i++){
+				ans[r+i] = argv[1][index5+i];
+			}
+			r += len;
+		}
+		index5 += len;
+	}
+	ans[r] = 0;
+	printf("Animal emojis: %s\n", ans);
+
+
 
     return 0;
 }
