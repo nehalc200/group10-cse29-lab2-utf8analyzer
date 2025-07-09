bdd3588cf8140d4ad85c1eb1219795f8ea1767f6
Choudhary <nechoudhary@ieng6-202.ucsd.edu>
2025-07-09 12:36:03 -0700

    function 2 and 4

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -11,4 +11,36 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
+    char *str = argv[1];
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 'a' + 'A';
+        }
+    }
+    printf("Uppercased ASCII: %s.\n", str);
+
+    int32_t count = 0;
+    int i = 0;
+
+    while (str[i] != '\0') {
+        unsigned char byte = (unsigned char)str[i];
+
+        if (byte <= 0x7F) {
+            i += 1;  
+        } else if ((byte & 0xE0) == 0xC0) {
+            i += 2;  
+        } else if ((byte & 0xF0) == 0xE0) {
+            i += 3;  
+        } else if ((byte & 0xF8) == 0xF0) {
+            i += 4;  
+        } else {
+            i += 1;  
+        }
+        count++;
+    }
+   
+    printf("Number of code points: %d\n", count);
+
+    return 0;
 }
