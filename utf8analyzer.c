2ed0016c9af18f7b8fadd15fff9ff7bb54110ec3
Singh <ars023@ieng6-203.ucsd.edu>
2025-07-09 12:37:25 -0700

    "added valid ascii"

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -11,4 +11,15 @@ int main(int argc, char *argv[]) {
     }
 
     // implement the UTF-8 analyzer here
+	int index1 = 0;
+	int j1 = 0;
+	while(argv[1][index1] != 0){
+		if (argv[1][index1] > 127 || argv[1][index1] < 0){
+			printf("Valid ASCII: %s\n", "false");
+			j1 = 1;
+			break;
+		}
+		index1++;
+	}
+	if (j1 == 0){ printf("Valid ASCII: %s\n", "true"); }
 }
